e0c1b14681421a90a6604b7e6aa96b7f9cde669b
aTKuehler <163857259+aTKuehler@users.noreply.github.com>
2025-01-09 14:26:54 +0100

    Add files via upload

---
+++ Java MaJoTrix.js
@@ -0,0 +1,17 @@
+var sternis = 0
+
+var klasse = "0"
+
+document.getElementById('sternis').innerText = "Sternis:" + sternis
+
+document.getElementById('hauptmenu').style.display='none'
+
+function zum_hauptmenu() {
+    klasse = document.getElementById('wahleklasse')
+    document.getElementById('klassen-auswahl').style.display = 'none'
+    document.getElementById('hauptmenu').style.display = 'block'
+}
+
+function lernen(){}
+
+function spielen(){}

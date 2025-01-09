937e82cf08484cab51b9ae2ea9de89884c76c872
T.Kühler <kuehlerschule@gmail.com>
2025-01-09 14:58:31 +0100

    Klasse speichern variable, noch nicht fertig 9.1.25

--- Java MaJoTrix.js
+++ Java MaJoTrix.js
@@ -1,13 +1,20 @@
-var sternis = 0
+let sternis = 0
 
-var klasse = "0"
 
 document.getElementById('sternis').innerText = "Sternis:" + sternis
 
 document.getElementById('hauptmenu').style.display='none'
 
+function klasse_speichern(){
+    let klasse = document.getElementById("wahleklasse").value;
+    console.log(klasse)
+    document.getElementById("klasse").innerText = "Ausgewählte Klasse: " + klasse
+    zum_hauptmenu()
+
+}
+
 function zum_hauptmenu() {
-    klasse = document.getElementById('wahleklasse')
+    var klasse = document.getElementById('wahleklasse')
     document.getElementById('klassen-auswahl').style.display = 'none'
     document.getElementById('hauptmenu').style.display = 'block'
 }

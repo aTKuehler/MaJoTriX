5661158a38bd602e5bcd9142fec45a262b467849
T.Kühler <kuehlerschule@gmail.com>
2025-01-13 14:50:12 +0100

    Lernen angefangen

--- Java MaJoTrix.js
+++ Java MaJoTrix.js
@@ -14,11 +14,35 @@ function klasse_speichern(){
 }
 
 function zum_hauptmenu() {
+    let klasse = document.getElementById('wahleklasse')
     document.getElementById('klassen-auswahl').style.display = 'none'
     document.getElementById('hauptmenu').style.display = 'block'
 }
 
-function lernen(){}
+function lernen(){
+    document.getElementById('hauptmenu').style.display = 'none';
+    document.getElementById('lernen').style.display = 'block';
+    neue_aufgaben();
+}
+
+function neue_aufgaben(){
+    for (let i = 0; i < 5; i++) {
+        let zahl1 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
+        let zahl2 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
+        let operator = Math.random() > 0.5 ? '+' : '-'; // Zufälliger Operator (+ oder -)
+        let richtige_antwort = (operator === '+') ? zahl1 + zahl2 : zahl1 - zahl2;
+}
+}
+
+
+
+
+
+
+
+
+
+
 
 function spielen(){}
 

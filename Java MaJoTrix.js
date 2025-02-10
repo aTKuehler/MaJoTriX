ba7422f78319ef05b4c7da317c3a6c610015cbd6
T.Kühler <kuehlerschule@gmail.com>
2025-02-10 14:49:26 +0100

    button lernen und verknüpfungen weiter entwickelt lernen weiter

--- Java MaJoTrix.js
+++ Java MaJoTrix.js
@@ -1,3 +1,5 @@
+//Tristan mit kuehlerschule anmelden
+let klasse = ""
 let sternis = 0
 
 
@@ -17,25 +19,38 @@ function zum_hauptmenu() {
     let klasse = document.getElementById('wahleklasse')
     document.getElementById('klassen-auswahl').style.display = 'none'
     document.getElementById('hauptmenu').style.display = 'block'
+    console.log(klasse)
 }
 
-function lernen(){
+function zu_lernen(){
     document.getElementById('hauptmenu').style.display = 'none';
-    document.getElementById('lernen').style.display = 'block';
-    neue_aufgaben();
+    document.getElementById('zu_lernen').style.display = 'block';
+
+    if (klasse === "1./2._klasse") {
+        neue_aufgaben_12()}
+    if (klasse === "3./4._klasse"){
+        neue_aufgaben_34()}
+
 }
 
-function neue_aufgaben(){
+function neue_aufgaben_12() {
     for (let i = 0; i < 5; i++) {
         let zahl1 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
         let zahl2 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
         let operator = Math.random() > 0.5 ? '+' : '-'; // Zufälliger Operator (+ oder -)
         let richtige_antwort = (operator === '+') ? zahl1 + zahl2 : zahl1 - zahl2;
+    }
+
 }
+
+function neue_aufgaben_34() {
+    for (let i = 0; i < 5; i++) {
+        let zahl1 = Math.floor(Math.random() * 50) + 1; // Zufallszahl zwischen 1 und 50
+        let zahl2 = Math.floor(Math.random() * 50) + 1; // Zufallszahl zwischen 1 und 50
+        let operator = Math.random() > 0.5 ? '+' : '-'; // Zufälliger Operator (+ oder -)
+        let richtige_antwort = (operator === '+') ? zahl1 + zahl2 : zahl1 - zahl2;
+    }
 }
-
-
-
 
 
 

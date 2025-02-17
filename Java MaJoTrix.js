797f7f850dd668e5f12cd6df44b09cb818bfcc70
T.Kühler <kuehlerschule@gmail.com>
2025-02-17 14:52:12 +0100

    funktion klassen_auswahl

--- Java MaJoTrix.js
+++ Java MaJoTrix.js
@@ -26,25 +26,38 @@ function zum_hauptmenu() {
 }
 
 function zu_lernen(){
-    document.getElementById('hauptmenu').style.display = 'none';
-    document.getElementById('lernen').style.display = 'block';
+    document.getElementById('klassen-auswahl').style.display = 'none'
+    document.getElementById('hauptmenu').style.display = 'none'
+    document.getElementById('lernen').style.display = 'block'
 
+    console.log("Aufgaben42")
+
+// HIER SCHEITERTS AN DER IF FUNKTION!!!
     if (klasse === "1./2._klasse") {
+        console.log("Aufgaben")
         neue_aufgaben_12()}
     if (klasse === "3./4._klasse"){
+        console.log("Aufgaben 3/4")
         neue_aufgaben_34()}
 
 }
 
 function neue_aufgaben_12() {
-    let antwort = {}
-    for (let i = 0; i < 5; i++) {
-        let zahl1 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
-        let zahl2 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
-        let operator = Math.random() > 0.5 ? '+' : '-'; // Zufälliger Operator (+ oder -)
+    let aufgaben = document.getElementById("aufgaben")
+    console.log("Aufgaben")
+    aufgaben.innerHTML = "hallo"
 
-        if (operator === "+"){let losung=zahl1 + zahl2}
+
+    for (let i = 0; i < 3; i++) {
+        let zahl1 = Math.floor(Math.random() * 10) + 1; // Zufallszahl zwischen 1 und 10
+        let zahl2 = Math.floor(Math.random() * zahl1) + 1; // Zufallszahl zwischen 1 und zahl1, Somit ist zahl2 kleiner als zahl1
+
+        aufgaben.innerHTML += "<p>${zahl1} + ${zahl2}<p>"
+
+
     }
+
+
 
 }
 
